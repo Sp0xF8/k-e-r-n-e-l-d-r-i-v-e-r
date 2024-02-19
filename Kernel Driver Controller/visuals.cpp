36875b2b3d7bf0ee786eff9a6a08942ecefcda33
Sp0xF8 <spoonnosaurus@gmail.com>
2024-02-19 00:17:47 +0000

    doesnt crash

--- Kernel Driver Controller/visuals.cpp
+++ Kernel Driver Controller/visuals.cpp
@@ -4,7 +4,7 @@
 #include "offsets.h"
 
 #include <string>
-
+#include <iostream>
 
 #include "config.h"
 
@@ -16,50 +16,68 @@ void Visuals::ESP() {
 	uintptr_t local_player = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, Data::client_dll + Offsets::Client::dwLocalPlayerPawn, sizeof(uintptr_t));
 
 	if (local_player == NULL) return;
-	
 
-	Draw::Text({ 10, 10 }, std::to_string(local_player).c_str(), Colour(255, 255, 255, 255), true);
+	int local_health = Data::Driver->ReadVirtualMemory<int>(Data::ProcessID, local_player + Offsets::Client::C_BaseEntity::m_iHealth, sizeof(int));
+
+	Draw::Text({ 10, 10 }, std::to_string(local_health).c_str(), Colour(255, 255, 255, 255), true);
 	
-	/*
 	uintptr_t pGameSceneNode = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, local_player + Offsets::Client::C_BaseEntity::m_pGameSceneNode, sizeof(uintptr_t));
+
+	if (pGameSceneNode == NULL) return;
 
 	Vector3 local_position = Data::Driver->ReadVirtualMemory<Vector3>(Data::ProcessID, pGameSceneNode + Offsets::Client::CGameSceneNode::m_vecOrigin, sizeof(Vector3));
 
+	Draw::Text({ 10, 30 }, std::to_string(local_position.x).c_str(), Colour(255, 255, 255, 255), true);
+	Draw::Text({ 10, 50 }, std::to_string(local_position.y).c_str(), Colour(255, 255, 255, 255), true);
+	Draw::Text({ 10, 70 }, std::to_string(local_position.z).c_str(), Colour(255, 255, 255, 255), true);
+
+
 	view_matrix_t view_matrix = Data::Driver->ReadVirtualMemory<view_matrix_t>(Data::ProcessID, Data::client_dll + Offsets::Client::dwViewMatrix, sizeof(view_matrix_t));
+
+	if (view_matrix[0][0] == NULL) return;
 
 	uintptr_t entity_list = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, Data::client_dll + Offsets::Client::dwEntityList, sizeof(uintptr_t));
 
+	if (entity_list == NULL) return;
+
 	int local_team = Data::Driver->ReadVirtualMemory<int>(Data::ProcessID, local_player + Offsets::Client::C_BaseEntity::m_iTeamNum, sizeof(int));
 
-	for (int entries = 1; entries < 32; entries++) {
+	if (local_team == NULL) return;
+
+	Draw::Text({ 10, 90 }, std::to_string(local_team).c_str(), Colour(255, 255, 255, 255), true);
+
+	
+
+
+	for (ULONG64 entities = 1; entities < 32; entities++) {
+
+		uintptr_t list_entry = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, entity_list + 0x10, sizeof(uintptr_t));
+
+		if (!list_entry) return;
+
+		uintptr_t player = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, list_entry + 120 * (entities & 0x1FF), sizeof(uintptr_t));
+
+		if (!player) continue;
+
+		int player_health = Data::Driver->ReadVirtualMemory<int>(Data::ProcessID, player + Offsets::Client::C_BaseEntity::m_iHealth, sizeof(int));
+
+		if(player_health <= 0 || player_health > 100) continue;
+
+		std::cout << player_health << std::endl;
+
+	}
 
 
 
-		
-		uintptr_t entity = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, entity_list + (8 * (entries & 0x7FFF) >> 9) + 16, sizeof(uintptr_t));
-
-		if (entity == NULL) continue;
-
-		uintptr_t entity_controller = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, entity + 120 * (entries & 0x1FF), sizeof(uintptr_t));
-
-		if (entity_controller == NULL) continue;
-
-		uintptr_t entity_controller_pawn = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, entity_controller + Offsets::Client::CCSPlayerController::m_hPlayerPawn, sizeof(uintptr_t));
-
-		if (entity_controller_pawn == NULL) continue;
-
-		entity = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, entity + (8 * ((entity_controller_pawn & 0x7FFF) >> 9) + 16), sizeof(uintptr_t));
-
-		if (entity == NULL) continue;
-
-		uintptr_t entity_pawn = Data::Driver->ReadVirtualMemory<uintptr_t>(Data::ProcessID, entity + (120) * (entity_controller_pawn & 0x1FF), sizeof(uintptr_t));
-
-		if (entity_pawn == NULL) continue;
-		
 
 
-		uint16_t entity_team = Data::Driver->ReadVirtualMemory<uint16_t>(Data::ProcessID, entity_pawn + Offsets::Client::C_BaseEntity::m_iTeamNum, sizeof(uint16_t));
+	/*
+	
 
+
+
+
+	
 		if (entity_team == local_team) continue;
 
 		int entity_health = Data::Driver->ReadVirtualMemory<int>(Data::ProcessID, entity_pawn + Offsets::Client::C_BaseEntity::m_iHealth, sizeof(int));
@@ -98,8 +116,8 @@ void Visuals::ESP() {
 
 
 	}
-
-
 	*/
+
+
 	
 }
